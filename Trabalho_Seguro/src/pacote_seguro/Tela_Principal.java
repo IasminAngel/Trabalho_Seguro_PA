872ea126aeafdc34199ec658951c026bee7d962e
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2024-05-15 14:03:38 -0300

    Update Tela_Principal.java

--- Trabalho_Seguro/src/pacote_seguro/Tela_Principal.java
+++ Trabalho_Seguro/src/pacote_seguro/Tela_Principal.java
@@ -1,3 +1,4 @@
+// Todos os imports necessarios
 package pacote_seguro;
 import javax.swing.JButton; 
 import javax.swing.JFrame;
@@ -6,11 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Tela_Principal {
-
 	public static void main(String[] args) {
 		
+		// Declarando subclasse funcionario
 		funcionario funcionario = new funcionario();
 		
+		// Puxando funcao da subclasse funcionario
 		funcionario.interface_funcionario(funcionario);
 		
 	}
